afa4749e00ee26245aa7f32c28f220b47558e837
lgeorge2095 <113368204+lgeorge2095@users.noreply.github.com>
2025-03-27 20:19:23 -0500

    New standard config file

---
+++ SOSGameConfig.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class SOSGameConfig {
+    public static final int MIN_BOARD_SIZE = 3;
+    public static final int MAX_BOARD_SIZE = 12;
+    public static final Color BLUE_PLAYER_COLOR = Color.BLUE;
+    public static final Color RED_PLAYER_COLOR = Color.RED;
+    public static final Color BLUE_SOS_HIGHLIGHT = new Color(200, 230, 255);
+    public static final Color RED_SOS_HIGHLIGHT = new Color(255, 220, 220);
+    
+    public static final String GAME_TITLE = "SOS Game";
+    public static final int DEFAULT_BOARD_SIZE = 3;
+    
+    private SOSGameConfig() {}
+}
